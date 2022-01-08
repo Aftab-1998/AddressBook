54755e9e2b05da74612b8d5e5fa93381257ac7f4
Aftab-1998 <alam41976@gmail.com>
2022-01-08 17:46:34 +0530

    [Aftab] Add; Added the contact details created

---
+++ DataMangement/DataMangement/AddressBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagement
+{
+    public class AddressBook
+    {
+        List<Contact> addressList = new List<Contact>();
+        public void Display() // This Method wii Display the Details
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Zip : " + contact.Zip);
+                Console.WriteLine("PhoneNumber : " + contact.PhoneNumber);
+                Console.WriteLine("Email : " + contact.Email);
+            }
+        }
+      
+    }
+}
---
+++ DataMangement/DataMangement/Program.cs
@@ -0,0 +1,33 @@
+﻿using DataManagement;
+
+class program
+{
+    Contact contact = new Contact();
+    static void Main(string[] args)
+    {
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Enter your Choice Number to Execute the Address Program Press- 1-Contacts");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the Contact Information in Format of FirstName, LastName,  City, State, Zip, Ph.No, Email.");
+                    Contact newContact = new Contact();
+                    newContact.FirstName = Console.ReadLine();
+                    newContact.LastName = Console.ReadLine();
+                    newContact.City = Console.ReadLine();
+                    newContact.State = Console.ReadLine();
+                    newContact.Zip = Console.ReadLine();
+                    newContact.PhoneNumber = Console.ReadLine();
+                    newContact.Email = Console.ReadLine();
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
+
+    }
+}
